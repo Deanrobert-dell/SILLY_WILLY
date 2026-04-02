b1f7ac18d780b1a6d2e15560d3550bd755e9269f
CupSlash <briggs.hardy@ucas-edu.net>
2026-04-02 16:09:45 +0000

    Adds my ortho code briggs.py. briggs.py creates an empty dict and creates three functions without calling them

---
+++ briggs.py
@@ -0,0 +1,27 @@
+#BH 2nd Ortho
+categories = [
+	{
+}
+]
+def track_savings_goal():
+	current_money = #FROM CSV#
+	necessary_money = input(“How much money would you like to save up to?”)
+	savings_time = input(“How long are you going to save for (in months)?”)
+	total = (necessary_money - current_money) / savings_time
+	print(“You will need to add ${total} to your savings each day to earn {necessary_money} in {savings_time} months?”)
+def check_budget_limit(categories):
+	for key in categories:
+		print(key)
+	category_to_check = input(“Which of the above categories would you like to check?”) 
+	If not categories['category_to_check']:
+		print(“That category does not exist.”)
+		return
+	print(f”The budget for that category is {categories['category_to_check']}.”)
+def add_budget_limit(categories):
+	for key in categories:
+		print(key)
+	category_needing_budget = input(“Which of the above categories would you like to check?”)
+	If not categories['category_needing_budget']:
+		print(“That category does not exist.”)
+		return
+	If categories['category_needing_budget'] != “None”:
