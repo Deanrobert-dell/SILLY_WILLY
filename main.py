591dd4e845fdc1111d70082e396d79fef151174f
Hunter <nathan.hasson@ucas-edu.net>
2026-04-09 16:41:51 +0000

    GAAAHHHHHHHH

--- main.py
+++ main.py
@@ -28,11 +28,12 @@ def main_menu():
 main_menu()
     # welcome user
 print("Welcome user!")
+while True:
     #print  each option ex:income budgeting, data visualizer
     # display the options           
     #user input asking which option
     #if statements corresponding to user input, call other functions
-        choice = input("What would you like to do? \n1. Create Savings Goal\n2. Check Budget Limit\n3. Add Budget Limit\n4. Expense Management\nE. Exit\n")
+        choice = input("What would you like to do? \n1. Create Savings Goal\n2. Check Budget Limit\n3. Add Budget Limit\n4. Expense Management\nE. Exit\n").strip()
         if choice == "1":
             create_savings_goal(user_id)
         elif choice == "2":
